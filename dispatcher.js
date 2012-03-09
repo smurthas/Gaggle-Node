bbbe35d4b3e2c2e48dbc60234457895a6fa98e64
Simon Murtha-Smith <simon@murtha-smith.com>
2012-03-08 18:24:15 -0700

    only the pass for redis, no username

--- dispatcher.js
+++ dispatcher.js
@@ -2,17 +2,22 @@ var config = require('config');
 var crawler = require('crawler');
 var redis = require('redis');
 var async = require('async');
+var providers = ['facebook','twitter','instagram'];
 
 var client = redis.createClient(config.redis.port, config.redis.host, {});
-if(config.redis.user && config.redis.pass) client.auth(config.redis.user, config.redis.pass);
-
-var providers = ['facebook','twitter','instagram'];
+if(config.redis.pass) client.auth(config.redis.pass, function(err) {
+  if(err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
 
 client.on('ready', function() {
   crawler.init(function() {
     for(var i = 0; i < config.redis.count; i++) process.nextTick(doCrawl);
   })
 });
+
 
 function getNext(callback) {
   client.rpop(config.redis.key, callback);
